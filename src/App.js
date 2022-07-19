b1fa62db0a36f83fe2fd197a609e880a22b443e3
Fombi Favour <fombifavour@gmail.com>
2022-07-19 20:11:34 +0200

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom';
 
-import NavBar from './components/NavBar'
+import Home from './routes/Home';
 
 const App = () => {
   return (
-    <NavBar />
-  )
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App
