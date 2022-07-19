b605808b8146dcf4ee32584671f5033c5d8e4c49
Fombi Favour <fombifavour@gmail.com>
2022-07-19 20:11:22 +0200

    creating navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {FaBars, FaTimes} from 'react-icons/fa';
 
-const NavBar = () => {
+import logo from '../assets/logo.png';
+import './NavbarStyles.css';
+
+const Navbar = () => {
+  const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    const [color, setColor] = useState(false)
+        const changeColor = () => {
+            if(window.scrollY >= 100){
+                setColor(true)
+            }else{
+                setColor(false)
+            }
+        }
+
+        window.addEventListener('scroll', changeColor)
+
+
   return (
-    <div>NavBar</div>
+    <div className={color ? 'header header-bg' : 'header'}>
+        <Link to='/'> <img src={logo} /> <h1>Learning Management System</h1> </Link>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li>
+                <Link to='/'>SIGN IN</Link>
+            </li>
+            <li>
+                <Link to='/'>REGISTER</Link>
+            </li>
+        </ul>
+
+        <div className='hamburger' onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{color: '#fff'}} />) : <FaBars size={20} style={{color: '#fff'}} />}
+        </div>
+    </div>
   )
 }
 
-export default NavBar
+export default Navbar
