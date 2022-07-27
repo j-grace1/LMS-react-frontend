01344d05a71785754249ae444cb1f0f2876400a8
j-grace1 <nchindejoshoua@gmail.com>
2022-07-27 10:30:29 +0100

    some setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/Home';
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </>
-  );
+    return ( <
+        >
+        <
+        Routes >
+        <
+        Route path = '/'
+        element = { < Home / > }
+        /> <
+        /Routes> <
+        />
+
+
+    );
 }
 
 export default App
